f126a2ce0e432db71ca20b2e32e66ffc2bc6ebf3
Alex Cristian <sevenalexandru69@gmail.com>
2023-04-23 17:20:50 +0300

    Added animations.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,38 @@
-// alert("TEST");
+window.onload = function () {
+    var delayTime = 800;
+    anime.timeline({ loop: false })
+        .add({
+            targets: '.name',
+            scale: [10, 1],
+            opacity: [0, 1],
+            easing: "easeOutCirc",
+            duration: 800,
+            delay: (el, i) => delayTime * i
+        });
+
+    var totalNameElements = document.getElementsByClassName('name').length;
+    setTimeout(function () {
+        addExternalLinks();
+    }, delayTime * totalNameElements);
+}
+
+
+function addExternalLinks() {
+    anime({
+        targets: '.githubLink',
+        translateX: ((window.innerWidth / 2) * (-1)),
+        opacity: [1, 0],
+        direction: 'reverse',
+        easing: 'easeInOutSine',
+        duration: 1600,
+    });
+
+    anime({
+        targets: '.linkedinLink',
+        translateX: (window.innerWidth / 2),
+        opacity: [1, 0],
+        direction: 'reverse',
+        easing: 'easeInOutSine',
+        duration: 1600,
+    });
+}
